b3b0df9c1a843eb61f894aa3cde04dd193aab711
Flor Tello <flor.26071994@gmail.com>
2017-03-06 16:48:39 -0500

    Creando Funcion para mostrar coders y regresar al inicio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
-var coders = [ { nombre : "Meche", apellido : "Zubieta",  promocion : "1era. promoción", estado : "Trabajando" },
+function mostrarDatos(){
+	var coders = [ { nombre : "Meche", apellido : "Zubieta",  promocion : "1era. promoción", estado : "Trabajando" },
 			 { nombre : "Arabela", apellido : "Rojas", promocion : "2da. promoción", estado : "Trabajando" },
 			 { nombre : "Maria", apellido : "Rosán",  promocion : "2da. promoción", estado : "Trabajando" },
 			 { nombre : "Guadalupe", apellido : "Racio",  promocion : "3era. promoción", estado : "Trabajando" },
@@ -6,6 +7,19 @@ var coders = [ { nombre : "Meche", apellido : "Zubieta",  promocion : "1era. pro
 			 { nombre : "Daniella", apellido : "Durán",  promocion : "4ta. promoción", estado : "Trabajando" },
 			 { nombre : "Michelle", apellido : "Seguil",  promocion : "5ta. promoción", estado : "Trabajando" },
 			 { nombre : "Diana", apellido : "Navarro",  promocion : "5ta. promoción", estado : "Trabajando" } ];
+
+	var mostrar = "<ul>";
+	coders.forEach(function(e){
+		mostrar += "<div class = 'div-coder'><li> Nombre: "+e.nombre+"<br /></li>"+
+				   "<li>Apellido: "+e.apellido+"<br /></li>"+
+				   "<li>Promoción: "+e.promocion+"<br /></li>"+
+				   "<li>Estado: "+e.estado+"<br /></li></div>";
+	});
+	mostrar += "</ul>";
+	document.write(mostrar);
+}
+mostrarDatos();
+
 var respuestas =[{p1 : 'p1' , p2 : 'p2' , p3 : 'p2'},{p1 : 'p1' , p2 : 'p2' , p3 : 'p2'},{p1 : 'p1' , p2 : 'p2' , p3 : 'p2'}]
 function login(){
 	var nombre = prompt("Cual es Nombre");
